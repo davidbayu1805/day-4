c2574281e326f771bbea922e4826204e46db4ac0
david bayu cristanto <davidbayu57@gmail.com>
2025-07-07 16:20:02 +0700

    day1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ const HomePage = () => {
   return (
     <div className="homepage pb-10 px-6 sm:px-8 md:px-12">
       <div className="container mx-auto">
-        {/* Hero Section */}
         <div className="hero pt-24 flex justify-center">
           <div className="box max-w-3xl w-full">
             <h1 className="lg:text-5xl/tight text-3xl font-medium mb-7">
@@ -13,7 +12,6 @@ const HomePage = () => {
             <p className="text-base/8 mb-7 text-justify">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis cupiditate rerum autem doloribus magnam at labore provident soluta laborum, odio accusamus maiores architecto mollitia. Deleniti laborum nam explicabo quae quas, deserunt est quos doloremque rerum vitae delectus, quis veritatis error adipisci dolor. Veniam eligendi pariatur dolorum delectus a adipisci nesciunt itaque, enim, accusantium provident ex impedit dicta veritatis ab asperiores, tempora odit! Fugiat quo id rerum odio magnam, nam odit.
             </p>
-            {/* Tombol Contact dan Download */}
             <div className="flex flex-col gap-3">
               <a
                 href="https://www.linkedin.com/in/david-bayu"
@@ -32,7 +30,6 @@ const HomePage = () => {
           </div>
         </div>
 
-        {/* Icon Sosial Media */}
         <div className="flex justify-center gap-6 text-2xl text-black mb-2">
           <a href="https://www.linkedin.com/in/david-bayu">
             <i className="ri-linkedin-box-fill hover:text-blue-600 transition"></i>
@@ -51,7 +48,6 @@ const HomePage = () => {
           </a>
         </div>
 
-        {/* Profile Section */}
         <div className="profile flex justify-center pt-5" id="Profile">
           <div className="profile-box w-full">
             <div className="box p-4 bg-white shadow max-w-xs rounded-2xl mx-auto">
